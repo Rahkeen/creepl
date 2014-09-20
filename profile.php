23763c26e4f34e10573eb7b3579828e6661112d0
JELGT2011 <jelgt2011@gmail.com>
2014-09-20 12:53:04 -0400

    profile php

---
+++ profile.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" ng-app>
+	<!--
+	Modified from the Debian original for Ubuntu
+	Last updated: 2014-03-19
+	See: https://launchpad.net/bugs/1288690
+	-->
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+		<link rel="stylesheet" type="text/css" href="bower_components/bootstrap/dist/css/bootstrap.css">
+  		<script src="bower_components/angular/angular.js"></script>
+  		
+		<title>Creepl</title>
+
+	</head>
+	<body>
+		<div class="main_page">
+			<div class="page_header floating_element">
+				<span class="floating_element"> Welcome to Creepl </span>
+			</div>
+	</body>
+</html>
+
